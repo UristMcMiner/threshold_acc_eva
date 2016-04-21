32333a51ec32a869ed8de8a59f8d39e3b1cf6549
fbaier <florian.baier@she.net>
2016-04-21 09:42:56 +0200

    test

--- src/threshold_ackley/main_fba.java
+++ src/threshold_ackley/main_fba.java
@@ -2,13 +2,22 @@ package threshold_ackley;
 
 public class main_fba {
 
+	public static double lowest_value;
+	public static double highest_value;
 	public static void main(String[] args) {
-		int nS = 1000;
-		int nT = 10;
-		int nD = 2000;
-		double q = 0.5;
+		double[] start = {1,1,1,1,1,1};
+		int dim = 6;
+		int t = 2000;
+		
+		
+		
 		
 
 	}
 
+	
+	
+	public static int ack_fitness(double val){
+		
+	}
 }
