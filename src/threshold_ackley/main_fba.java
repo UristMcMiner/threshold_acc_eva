c383ee35e054020a34f1c70eb29d9b5d46f3304f
fbaier <florian.baier@she.net>
2016-04-21 10:38:20 +0200

    fitness

--- src/threshold_ackley/main_fba.java
+++ src/threshold_ackley/main_fba.java
@@ -6,6 +6,8 @@ public class main_fba {
 	public static double highest_value;
 	public static double[] lowest_deviation;
 	public static double[] highest_deviation;
+	public static int best_fitness = 1;
+	public static double[] fittest_deviation;
 	public static void main(String[] args) {
 		int q = 1;
 		int q_new = 1;
@@ -17,6 +19,8 @@ public class main_fba {
 		int max = 40;
 		int min = -40;
 		double u = 0.5;
+		int de = 0;
+		int qual = 0;
 		int iterations = 50000000;
 		double[] deviated = start;
 		lowest_value = ackley_func.ackley_func(start);
@@ -25,17 +29,10 @@ public class main_fba {
 		highest_deviation = start;
 		for(int i = 0; i < iterations; i++){
 			deviated = deviation(deviated, max, min, u);
-			double value = ackley_func.ackley_func(deviated);
-			if(value < lowest_value){
-				lowest_value = value;
-				lowest_deviation = deviated;
-			}
-			if(value > highest_value){
-				highest_value = value;
-				highest_deviation = deviated;
-			}
+			qual = ack_fitness(ackley_func.ackley_func(deviated));
+			de = 
 			
-			//System.out.println("{"+deviated[0]+","+deviated[1]+","+deviated[2]+","+deviated[3]+","+deviated[4]+","+deviated[5]+"}");
+			
 		}
 		System.out.println(lowest_value);
 		System.out.println("{"+lowest_deviation[0]+","+lowest_deviation[1]+","+lowest_deviation[2]+","+lowest_deviation[3]+","+lowest_deviation[4]+","+lowest_deviation[5]+"}");
@@ -49,13 +46,16 @@ public class main_fba {
 		double[] result = input;
 		for(int i = 0; i < input.length; i++)
 		{
-			result[i] = input[i] + Math.random()*0.00001;
+			result[i] = input[i] + (Math.random()*2-1)*0.1;
 			if(result[i]>40)result[i]=-40;
+			if(result[i]<-40)result[i]=40;
 		}
 		return result;
 	}
 	
-	/*public static int ack_fitness(double val){
-		if(val < )
-	}*/
+public static int ack_fitness(double val){
+		if(val==0)return 1000;
+		int x = (int) ((1 - (val/30)) * 1000);
+		return x;
+	}
 }
