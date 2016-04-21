c95e803057ba86a2240e2b14a4ba046b948f24e0
fbaier <florian.baier@she.net>
2016-04-21 09:08:53 +0200

    test

---
+++ src/threshold_ackley/main_fba.java
@@ -0,0 +1,14 @@
+package threshold_ackley;
+
+public class main_fba {
+
+	public static void main(String[] args) {
+		int nS = 1000;
+		int nT = 10;
+		int nD = 2000;
+		double q = 0.5;
+		
+
+	}
+
+}
